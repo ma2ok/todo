49cbd5c56e455ecc4d2bc74b498829895663b3ec
P <>
2020-11-11 19:43:52 +0900

    Add : 最初の環境構築

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+export default () => {
+  return <>
+    <div>about page</div>
+    <Link href='/'><a>Home</a></Link>
+  </>
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Home = () => {
+  return <div>
+    <div>Welcome to Next.js!</div>
+    <Link href='/about'><a>about</a></Link>
+  </div>
+}
+
+export default Home
