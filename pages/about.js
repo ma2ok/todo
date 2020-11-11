209b702a9953f5a9165720fb4dd747ffd2ce7183
ma2ok <workwing0283@gmail.com>
2020-11-11 20:15:54 +0900

    Add : prettier追加

--- pages/about.js
+++
@@ -1,8 +0,0 @@
-import Link from 'next/link'
-
-export default () => {
-  return <>
-    <div>about page</div>
-    <Link href='/'><a>Home</a></Link>
-  </>
-}
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default () => {
+  return (
+    <>
+      <div>about page</div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </>
+  );
+};
